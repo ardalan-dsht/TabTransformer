2d1091cb13495b82f3db2db7fd953a3be2e4d79a
Ardalan Daneshvar Tarigh <ardalan.dsht@gmail.com>
2024-02-21 15:15:18 +0330

    reformat test_encoder.py

--- tests/unit/test_encoder.py
+++ tests/unit/test_encoder.py
@@ -9,13 +9,9 @@ class TestEncoder(unittest.TestCase):
     def test1(self):
         x = torch.rand((2, 12, 12))
         encoder1 = Encoder(12, 3, 1024, 4, 0.5)
-        self.assertEqual(
-            encoder1(x).shape, torch.Size([2, 12, 12])
-        )
+        self.assertEqual(encoder1(x).shape, torch.Size([2, 12, 12]))
 
     def test2(self):
         x = torch.rand((2, 14, 14))
         encoder2 = Encoder(14, 7, 256, 1, 0.0)
-        self.assertEqual(
-            encoder2(x).shape, torch.Size([2, 14, 14])
-        )
+        self.assertEqual(encoder2(x).shape, torch.Size([2, 14, 14]))
