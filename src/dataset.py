32d4b1f27ed18071828cf683cc846757c0a689c3
Ardalan Daneshvar Tarigh <ardalan.dsht@gmail.com>
2024-02-24 15:22:12 +0330

    add docstring to dataset

--- src/dataset.py
+++ src/dataset.py
@@ -7,6 +7,37 @@ from torch.utils.data import Dataset
 
 
 class TabTransformerDataset(Dataset):
+    """
+    A custom PyTorch Dataset class for handling tabular data for TabTransformer models.
+
+    Args:
+        labels (pd.Series): The target labels for the dataset.
+        categorical_dataset (pd.DataFrame): The categorical features dataset.
+        numerical_dataset (Optional[pd.DataFrame], optional): The numerical features dataset. Defaults to None.
+
+    Attributes:
+        categorical_dataset (pd.DataFrame): The categorical features dataset.
+        numerical_dataset (Optional[pd.DataFrame]): The numerical features dataset if provided.
+        model_type (str): Type of model based on the presence of numerical features.
+        labels (pd.Series): The target labels for the dataset.
+
+    Methods:
+        __init__(self, labels: pd.Series, categorical_dataset: pd.DataFrame, numerical_dataset: Optional[pd.DataFrame] = None):
+            Initializes the TabTransformerDataset with the provided datasets and labels.
+
+        __len__(self) -> int:
+            Returns the length of the dataset based on the categorical features.
+
+        __getitem__(self, index: int) -> tuple:
+            Retrieves a sample from the dataset at the specified index.
+            Returns a tuple containing categorical sample, label sample, and numerical sample if present.
+
+    Usage:
+    dataset = TabTransformerDataset(labels, categorical_data, numerical_data)
+    dataloader = DataLoader(dataset, batch_size=32, shuffle=True)
+    for batch in dataloader:
+        # Perform operations on batch
+    """
     def __init__(
         self,
         labels: pd.Series,
